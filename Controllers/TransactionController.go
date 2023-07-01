e98c36b28664abd60a3cd747cdaadd6be7797475
GLAZERadr <adrianbadjideh@student.telkomuniversity.ac.id>
2023-07-01 17:07:47 +0800

    MAKE ROUTE TO DATABASE

--- Controllers/TransactionController.go
+++ Controllers/TransactionController.go
@@ -9,35 +9,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// func GetOneTranscation (c *fiber.Ctx) error {
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 	transactionId := c.params("transactionId")
-// 	var transaction Entity.Transaction
-// 	defer cancel()
-
-// 	objId, _ := primitive.ObjectIDFromHex(transactionId)
-
-// 	err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&transaction)
-// 	if err != nil {
-// 		return c.Status(http.StatusInternalServerError).JSON(response.TransactionResponse{
-// 			Status: http.StatusInternalServerError,
-// 			Message: "error",
-// 			Data: &fiber.Map{"data": err.Error()}
-// 		})
-// 	}
-
-// 	return c.status(http.StatusOK).JSON(response.TransactionResponse{
-// 		Status: http.StatusOK,
-// 		Message : "success",
-// 		Data: &fiber.Map{"data": transaction}
-// 	})
-// 
-
 func GetAllTransactions(c *fiber.Ctx) error {
-	db := database.ConnectDB() // Mengambil koneksi database dari package database
+	db := database.ConnectDB()
 	defer db.Disconnect(context.Background())
 
-	collection := database.GetCollection(db, "Transaction") // Mendapatkan objek koleksi "Transaction"
+	client := database.GetDB() // Mengambil koneksi database dari package database
+
+	collection := database.GetCollection(client, "Transaction") // Mendapatkan objek koleksi "Transaction"
 
 	var transactions []Entity.Transaction
 	cursor, err := collection.Find(context.Background(), options.Find())
